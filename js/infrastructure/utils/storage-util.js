545b43c42381e86faa875e04e35ee59aab44e743
tmpmachine <bill.yosafatz@gmail.com>
2025-02-12 16:11:01 +0700

    Update storage-util.js

--- js/infrastructure/utils/storage-util.js
+++ js/infrastructure/utils/storage-util.js
@@ -1,110 +1,194 @@
-/* v1 */
-let storageUtil = (function() {
+// @ts-check
+/* v1.2 */
+let storageUtil = (function () {
+	// # vars
 
-  let db = null;
-  let dbVersion = 1;
-  let storeName = 'public';
-  let dbName = '';
+	let db = null;
+	let isDbClosed = true;
+	let dbVersion = 1;
+	let storeName = 'public';
+	let dbName = '';
+	let dbReadyPromiseResolver = null;
+	let dbReadyPromise = new Promise((resolve) => {
+		dbReadyPromiseResolver = resolve;
+	});
 
-  let SELF = {
-    Init,
-    openDb_,
-    setItem_,
-    getItem_,
-    removeItem_,
-    clearStore_,
-  };
+	// # function
 
-  // # init
-  function Init(_dbName, _dbVersion) {
-    dbName = _dbName;
-    dbVersion = _dbVersion;
-  }
-  
-  // # Clear all items from a specific object store
-  async function clearStore_() {
-    return new Promise(async (resolve, reject) => {
-      await openDb_(); // Ensure the database is open
-      const transaction = db.transaction([storeName], 'readwrite');
-      const store = transaction.objectStore(storeName);
-      const request = store.clear();
-  
-      request.onsuccess = () => resolve(true);
-      request.onerror = (event) => reject('Error clearing store: ' + event.target.errorCode);
-    });
-  }
+	async function openDb_(version, onUpgrade) {
+		return new Promise((resolve, reject) => {
+			if (!isDbClosed && (version ?? dbVersion) === dbVersion) resolve(db);
 
-  // # Open IndexedDB
-  async function openDb_() {
-    return new Promise((resolve, reject) => {
-      if (db) resolve(db);
+			const request = indexedDB.open(dbName, version ?? dbVersion);
+			request.onerror = (event) => reject('Error opening database: ' + event.target.errorCode);
+			request.onsuccess = (event) => {
+				db = event.target.result;
+				isDbClosed = false;
+				resolve(db);
+			};
+			request.onupgradeneeded = (event) => {
+				db = event.target.result;
+				onupgradeneeded(db);
 
-      const request = indexedDB.open(dbName, dbVersion);
-      request.onerror = (event) => reject('Error opening database: ' + event.target.errorCode);
-      request.onsuccess = (event) => {
-        db = event.target.result;
-        resolve(db);
-      };
-      request.onupgradeneeded = (event) => {
-        db = event.target.result;
-        // Create object stores if they don't exist
-        if (!db.objectStoreNames.contains(storeName)) {
-          db.createObjectStore(storeName, { keyPath: 'key' });
-        }
-      };
-    });
-  }
+				onUpgrade?.(db);
+			};
+		});
+	}
 
-  // # Set Item in IndexedDB
-  async function setItem_(key, value) {
-    return new Promise(async (resolve, reject) => {
-      await openDb_();
-      const transaction = db.transaction([storeName], 'readwrite');
-      const store = transaction.objectStore(storeName);
-      const request = store.put({ key, value });
+	function onupgradeneeded(db) {
+		if (!db.objectStoreNames.contains(storeName)) {
+			db.createObjectStore(storeName, { keyPath: 'key' });
+		}
+	}
 
-      request.onsuccess = () => {
-        resolve(true);
-      };
-      request.onerror = (event) => reject('Error storing item: ' + event.target.errorCode);
-    });
-  }
+	function getDatabaseVersion_() {
+		return new Promise((resolve, reject) => {
+			const request = indexedDB.open(dbName);
 
-  // # Get Item from IndexedDB or fallback to localStorage
-  async function getItem_(key) {
-    return new Promise(async (resolve, reject) => {
-      await openDb_();
-      const transaction = db.transaction([storeName], 'readonly');
-      const store = transaction.objectStore(storeName);
-      const request = store.get(key);
+			request.onsuccess = (event) => {
+				const db = event.target.result;
+				const version = db.version;
 
-      request.onsuccess = (event) => {
-        const result = event.target.result;
-        if (result) {
-          resolve(result.value);
-        } else {
-          resolve(null); // return null if not found
-        }
-      };
-      request.onerror = (event) => reject('Error retrieving item: ' + event.target.errorCode);
-    });
-  }
+				db.close();
 
-  // # Remove Item from IndexedDB and localStorage
-  async function removeItem_(key) {
-    return new Promise(async (resolve, reject) => {
-      await openDb_();
-      const transaction = db.transaction([storeName], 'readwrite');
-      const store = transaction.objectStore(storeName);
-      const request = store.delete(key);
+				isDbClosed = true;
 
-      request.onsuccess = () => {
-        resolve(true);
-      };
-      request.onerror = (event) => reject('Error removing item: ' + event.target.errorCode);
-    });
-  }
+				resolve(version);
+			};
 
-  return SELF;
+			request.onerror = (event) => {
+				reject(`Error opening database: ${event.target.errorCode}`);
+				isDbClosed = true;
+				db?.close();
+			};
 
+			request.onupgradeneeded = (event) => {
+				db = event.target.result;
+				onupgradeneeded(db);
+				resolve(null);
+			};
+		});
+	}
+
+	// # self
+	let SELF = {
+		openDb_,
+		getDatabaseVersion_,
+
+		// # init
+		async Init_(_dbName) {
+			dbName = _dbName;
+
+			dbVersion = (await getDatabaseVersion_()) ?? 1;
+			dbReadyPromiseResolver();
+		},
+
+		setDBVersion(newDBVersion) {
+			dbVersion = newDBVersion;
+		},
+
+		getDb() {
+			return db;
+		},
+
+		async checkStoreExists_(storeName) {
+			await dbReadyPromise;
+			await openDb_();
+			return db.objectStoreNames.contains(storeName);
+		},
+
+		async clearStore_() {
+			return new Promise(async (resolve, reject) => {
+				await openDb_(); // Ensure the database is open
+				const transaction = db.transaction([storeName], 'readwrite');
+				const store = transaction.objectStore(storeName);
+				const request = store.clear();
+
+				request.onsuccess = () => {
+					resolve(true);
+					db.close();
+					isDbClosed = true;
+				};
+				request.onerror = (event) => {
+					reject('Error clearing store: ' + event.target.errorCode);
+					db.close();
+					isDbClosed = true;
+				};
+			});
+		},
+
+		closeDb() {
+			isDbClosed = true;
+			db.close();
+		},
+
+		async setItem_(key, value) {
+			return new Promise(async (resolve, reject) => {
+				await openDb_();
+				const transaction = db.transaction([storeName], 'readwrite');
+				const store = transaction.objectStore(storeName);
+				const request = store.put({ key, value });
+
+				request.onsuccess = () => {
+					resolve(true);
+					db.close();
+					isDbClosed = true;
+				};
+				request.onerror = (event) => {
+					reject('Error storing item: ' + event.target.errorCode);
+					db.close();
+					isDbClosed = true;
+				};
+			});
+		},
+
+		// # get
+		async getItem_(key) {
+			return new Promise(async (resolve, reject) => {
+				await openDb_();
+				const transaction = db.transaction([storeName], 'readonly');
+				const store = transaction.objectStore(storeName);
+				const request = store.get(key);
+
+				request.onsuccess = (event) => {
+					const result = event.target.result;
+					if (result) {
+						resolve(result.value);
+					} else {
+						resolve(null); // return null if not found
+					}
+					db.close();
+					isDbClosed = true;
+				};
+				request.onerror = (event) => {
+					reject('Error retrieving item: ' + event.target.errorCode);
+					db.close();
+					isDbClosed = true;
+				};
+			});
+		},
+
+		// # remove
+		async removeItem_(key) {
+			return new Promise(async (resolve, reject) => {
+				await openDb_();
+				const transaction = db.transaction([storeName], 'readwrite');
+				const store = transaction.objectStore(storeName);
+				const request = store.delete(key);
+
+				request.onsuccess = () => {
+					resolve(true);
+					db.close();
+					isDbClosed = true;
+				};
+				request.onerror = (event) => {
+					reject('Error removing item: ' + event.target.errorCode);
+					db.close();
+					isDbClosed = true;
+				};
+			});
+		},
+	};
+
+	return SELF;
 })();
