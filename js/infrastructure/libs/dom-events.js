3f09d68cb795e934f570c97a7462cca96f5a1d51
tmpmachine <bill.yosafatz@gmail.com>
2025-03-04 11:37:41 +0700

    deprecate view state factory, add view state model

--- js/infrastructure/libs/dom-events.js
+++ js/infrastructure/libs/dom-events.js
@@ -1,76 +1,79 @@
-/* v1.2 */ 
+/* v1.2 */
 let DOMEvents = (function () {
+	let local = {
+		commonEventTypes: {
+			onclick: 'click',
+			ondblclick: 'dblclick',
+			onmousedown: 'mousedown',
+			onmouseup: 'mouseup',
+			onmousemove: 'mousemove',
+			onmouseover: 'mouseover',
+			onmouseout: 'mouseout',
+			ontouchstart: 'touchstart',
 
-    let local = {
-        commonEventTypes: {
-            onclick: 'click',
-            ondblclick: 'dblclick',
-            onmousedown: 'mousedown',
-            onmouseup: 'mouseup',
-            onmousemove: 'mousemove',
-            onmouseover: 'mouseover',
-            onmouseout: 'mouseout',
-            ontouchstart: 'touchstart',
+			onkeypress: 'keypress',
+			onkeydown: 'keydown',
+			onkeyup: 'keyup',
+			onfocus: 'focus',
+			onblur: 'blur',
 
-            onkeypress: 'keypress',
-            onkeydown: 'keydown',
-            onkeyup: 'keyup',
-            onfocus: 'focus',
-            onblur: 'blur',
+			oninput: 'input',
+			onchange: 'change',
+			onsubmit: 'submit',
+			onreset: 'reset',
+		},
+	};
 
-            oninput: 'input',
-            onchange: 'change',
-            onsubmit: 'submit',
-            onreset: 'reset'
-        },
-    };
+	let attachListeners = function (attr, eventType, callbacks, containerEl) {
+		let elements = containerEl.querySelectorAll(`[${attr}]`);
+		for (let el of elements) {
+			let callbackFunc = callbacks?.[el.getAttribute(attr)];
+			if (!callbackFunc) continue;
 
-    let attachListeners = function (attr, eventType, callbacks, containerEl) {
-        let elements = containerEl.querySelectorAll(`[${attr}]`);
-        for (let el of elements) {
-            let callbackFunc = callbacks?.[el.getAttribute(attr)];
-            if (!callbackFunc) continue;
+			el.addEventListener(eventType, callbackFunc);
+		}
+	};
 
-            el.addEventListener(eventType, callbackFunc);
-        }
-    };
-
-    /** 
+	/** 
         @param {any} eventsMap 
         @param {HTMLElement} containerEl - default = document 
     */
-    function Listen(eventsMap, containerEl = document) {
-        let { groupKey } = eventsMap;
-        let infix = groupKey ? `-${groupKey}` : '';
+	function Listen(eventsMap, containerEl = document) {
+		let { groupKey } = eventsMap;
+		let infix = groupKey ? `-${groupKey}` : '';
 
-        for (let key in eventsMap) {
-            if (key == 'groupKey') continue;
+		for (let key in eventsMap) {
+			if (key == 'groupKey') continue;
 
-            let callbackMap = eventsMap[key];
-            let eventType = callbackMap.eventType ?? getCommonEventType(key);
+			let callbackMap = eventsMap[key];
+			let eventType = callbackMap.eventType ?? getCommonEventType(key);
 
-            if (!eventType) {
-                console.error('Event type not defined:', key);
-                continue;
-            }
-            attachListeners(`data${infix}-${key}`, eventType, callbackMap, containerEl);
-        }
-    }
+			if (!eventType) {
+				console.error('Event type not defined:', key);
+				continue;
+			}
+			attachListeners(
+				`data${infix}-${key}`,
+				eventType,
+				callbackMap,
+				containerEl
+			);
+		}
+	}
 
-    function getCommonEventType(key) {
-        return local.commonEventTypes[key];
-    }
+	function getCommonEventType(key) {
+		return local.commonEventTypes[key];
+	}
 
-    function Configure(commonEventTypes) {
-        local.commonEventTypes = {
-            ...local.commonEventTypes,
-            ...commonEventTypes,
-        }
-    }
+	function Configure(commonEventTypes) {
+		local.commonEventTypes = {
+			...local.commonEventTypes,
+			...commonEventTypes,
+		};
+	}
 
-    return {
-        Configure,
-        Listen,
-    };
-
+	return {
+		Configure,
+		Listen,
+	};
 })();
---
+++ js/infrastructure/models/view-state.js
@@ -0,0 +1,139 @@
+class ViewState {
+	/**
+	 * @typedef {Object} ViewStateOption
+	 * @property {string} containerSelector
+	 * @property {string} viewDataKey
+	 * @property {number} defaultTransitionTimeout
+	 * @property {function(ViewData): void} [onBeforeShow]
+	 * @property {function(ViewData): void} [onShow]
+	 * @property {function(ViewData): void} [onHide]
+	 */
+
+	/**
+	 * @typedef {Object} ViewData
+	 * @property {HTMLElement} node
+	 * @property {string} viewName
+	 * @property {boolean} [isFirstRender]
+	 */
+
+	/** @param {ViewStateOption} opt */
+	constructor(opt) {
+		this.isAnimationStart = false;
+		this.containerSelector = opt.containerSelector.trim();
+		this.viewDataKey = opt.viewDataKey.trim();
+		this.onHide = opt.onHide;
+		this.onShow = opt.onShow;
+		this.onBeforeShow = opt.onBeforeShow;
+		this.defaultTransitionTimeout = opt.transitionTimeout ?? 1;
+	}
+
+	GetActiveNode() {
+		return document.querySelector(
+			`._rootViewContainer > [${this.viewDataKey}]:not(template)`
+		);
+	}
+
+	async Update_({ name, transitionTimeout }) {
+		if (this.isAnimationStart) return;
+
+		if (!transitionTimeout) {
+			transitionTimeout = this.defaultTransitionTimeout;
+		}
+		let nodes = Array.from(
+			document.querySelectorAll(`._rootViewContainer > [${this.viewDataKey}]`)
+		);
+
+		let targetNode =
+			nodes.find((node) => node.getAttribute(this.viewDataKey) == name) ??
+			nodes.find((node) => node.tagName != 'TEMPLATE') ??
+			nodes[0];
+		let targetViewName = targetNode.getAttribute(this.viewDataKey);
+
+		if (targetNode.tagName == 'TEMPLATE') {
+			let isAlreadyActive = nodes.some(
+				(node) =>
+					node.getAttribute(this.viewDataKey) == targetViewName &&
+					node.tagName != 'TEMPLATE'
+			);
+			if (isAlreadyActive) return;
+		}
+
+		let promises = [];
+
+		nodes
+			.filter((node) => node.tagName != 'TEMPLATE')
+			.forEach(async (node) => {
+				let viewName = node.getAttribute(this.viewDataKey);
+				if (viewName == targetViewName) return; // active view
+
+				let anchorNode = nodes.find(
+					(t) =>
+						t.tagName == 'TEMPLATE' &&
+						t.getAttribute(this.viewDataKey) == viewName
+				);
+
+				if (!anchorNode) {
+					anchorNode = document.createElement('template');
+					anchorNode.setAttribute(this.viewDataKey, viewName);
+					node.insertAdjacentElement('beforebegin', anchorNode);
+				}
+
+				node.dataset.hidden = true;
+
+				let hidePromise = new Promise(async (resolve) => {
+					await new Promise((resolve) =>
+						window.setTimeout(resolve, transitionTimeout)
+					);
+					node.removeAttribute(this.viewDataKey);
+					this.onHide?.(
+						/** @type {ViewData} */ ({
+							node,
+							viewName: viewName,
+						})
+					);
+					anchorNode.content.append(node);
+					resolve();
+				});
+				promises.push(hidePromise);
+			});
+
+		await Promise.all(promises);
+
+		this.isAnimationStart = true;
+
+		let node = targetNode.content?.firstElementChild ?? targetNode;
+
+		if (node) {
+			node.dataset.rviewName = targetViewName;
+
+			let isFirstRender = !node.dataset.rendered;
+			/** @type {ViewData} */
+			let viewData = {
+				node,
+				isFirstRender,
+				viewName: targetViewName,
+			};
+
+			this.onBeforeShow?.(viewData);
+
+			targetNode.parentNode.append(node); // highest element order
+
+			this.onShow?.(viewData);
+
+			node.dataset.rendered = true;
+
+			// delay 1ms to allow browser to render initial element state before starting transition
+			window.setTimeout(() => {
+				delete node.dataset.hidden;
+			}, 1);
+		}
+
+		await new Promise((resolve) =>
+			setTimeout(() => {
+				resolve();
+			}, transitionTimeout)
+		);
+
+		this.isAnimationStart = false;
+	}
+}
