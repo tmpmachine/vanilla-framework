f52bc8d31e78fb090931d5fcbfa912d2535a970d
tmpmachine <bill.yosafatz@gmail.com>
2025-03-05 14:04:35 +0700

    update idb models

--- js/infrastructure/models/idb-wrapper.js
+++ js/infrastructure/models/idb-wrapper.js
@@ -1,71 +1,164 @@
 // @ts-check
 
-// v2
+// v3.0
+
+/**
+ * @template T
+ * @class
+ */
 class IdbWrapper {
-	// # const
-	constructor(dbName, dbVersion = 1, upgradeCallback) {
-		this.db = null;
-		this.dbVersion = dbVersion;
-		this.dbName = dbName;
-		this.requestQueue = [];
-		this.requestResolverQueue = [];
-		this.upgradeCallback = upgradeCallback;
-		this.queueCounter = 0;
+	constructor(dbConnection, storeName) {
+		this.dbConnection = dbConnection;
+		this.storeName = storeName;
 	}
 
-	// # function
+	// # clear
+	/** @returns {Promise<boolean>} */
+	async Clear_() {
+		return new Promise(async (resolve, reject) => {
+			let dbRequestResult = await this.OpenDb_();
+			let { db, requestId } = dbRequestResult;
+			const transaction = db.transaction([this.storeName], 'readwrite');
+			const store = transaction.objectStore(this.storeName);
+			const request = store.clear();
 
-	// # self, # public
-	EndRequest(id) {
-		let requestIndex = this.requestResolverQueue.findIndex((e) => e.id == id);
+			request.onsuccess = () => {
+				resolve(true);
+				this.dbConnection.EndRequest(requestId);
+			};
+			request.onerror = (event) => {
+				reject('Error clearing store: ' + event.target.errorCode);
+				this.dbConnection.EndRequest(requestId);
+			};
+		});
+	}
 
-		this.requestQueue.splice(requestIndex, 1);
-		let [requestPromise] = this.requestResolverQueue.splice(requestIndex, 1);
+	// # remove, # delete
 
-		this.db.close();
+	/** @returns {Promise<boolean>} */
+	async Delete_(key) {
+		return new Promise(async (resolve, reject) => {
+			let dbRequestResult = await this.OpenDb_();
+			let { db, requestId } = dbRequestResult;
 
-		requestPromise.resolver();
+			try {
+				const transaction = db.transaction([this.storeName], 'readwrite');
+				const store = transaction.objectStore(this.storeName);
+				const request = store.delete(key);
+
+				request.onsuccess = () => {
+					resolve(true);
+					this.dbConnection.EndRequest(requestId);
+				};
+				request.onerror = (event) => {
+					throw event.target.errorCode;
+				};
+			} catch (error) {
+				this.dbConnection.EndRequest(requestId);
+				reject('Error storing item: ' + error);
+			}
+		});
+	}
+
+	// # add, # put
+	/**
+	 * @param {T} item
+	 * @returns {Promise<T>}
+	 */
+	Put_(item) {
+		return new Promise(async (resolve, reject) => {
+			let dbRequestResult = await this.OpenDb_();
+			let { db, requestId } = dbRequestResult;
+
+			try {
+				const transaction = db.transaction([this.storeName], 'readwrite');
+				const store = transaction.objectStore(this.storeName);
+				const request = store.put(item);
+
+				request.onsuccess = () => {
+					resolve(item);
+					this.dbConnection.EndRequest(requestId);
+				};
+				request.onerror = (event) => {
+					throw event.target.errorCode;
+				};
+			} catch (error) {
+				this.dbConnection.EndRequest(requestId);
+				reject('Error storing item: ' + error);
+			}
+		});
+	}
+
+	// # list, # get all
+	/** @returns {Promise<T[]>} */
+	List_() {
+		return new Promise(async (resolve, reject) => {
+			let items = [];
+			let dbRequestResult = await this.OpenDb_();
+			let { db, requestId } = dbRequestResult;
+			const transaction = db.transaction([this.storeName], 'readonly');
+
+			try {
+				const store = transaction.objectStore(this.storeName);
+				let cursorRequest = store.openCursor();
+
+				cursorRequest.onsuccess = (event) => {
+					let cursor = event.target.result;
+
+					if (cursor) {
+						items.push(cursor.value);
+						cursor.continue();
+					} else {
+						resolve(items);
+						this.dbConnection.EndRequest(requestId);
+					}
+				};
+
+				cursorRequest.onerror = (event) => {
+					throw event.target.errorCode;
+				};
+			} catch (error) {
+				this.dbConnection.EndRequest(requestId);
+				reject('Error retrieving items: ' + error);
+			}
+		});
+	}
+
+	// # get
+	/** @returns {Promise<T|null>} */
+	Get_(key) {
+		return new Promise(async (resolve, reject) => {
+			let dbRequestResult = await this.OpenDb_();
+			let { db, requestId } = dbRequestResult;
+
+			try {
+				const transaction = db.transaction([this.storeName], 'readonly');
+				const store = transaction.objectStore(this.storeName);
+				const request = store.get(key);
+
+				request.onsuccess = (event) => {
+					const result = event.target.result;
+
+					if (result) {
+						resolve(result);
+					} else {
+						resolve(null);
+					}
+
+					this.dbConnection.EndRequest(requestId);
+				};
+				request.onerror = (event) => {
+					throw event.target.errorCode;
+				};
+			} catch (error) {
+				this.dbConnection.EndRequest(requestId);
+				reject('Error retrieving item: ' + error);
+			}
+		});
 	}
 
 	async OpenDb_() {
-		let lastRequestPromise;
-
-		if (this.requestQueue.length > 0) {
-			let lastRequest = this.requestQueue[this.requestQueue.length - 1];
-			lastRequestPromise = lastRequest.promise;
-		}
-
-		let requestId = this.queueCounter++;
-
-		// add to queue
-		let newRequestPromise = new Promise((resolve) => {
-			this.requestResolverQueue.push({
-				id: requestId,
-				resolver: resolve,
-			});
-		});
-		this.requestQueue.push({
-			promise: newRequestPromise,
-			id: requestId,
-		});
-
-		await lastRequestPromise;
-
-		return new Promise((resolve, reject) => {
-			const request = indexedDB.open(this.dbName, this.dbVersion);
-			request.onerror = (event) => reject('Error opening database: ' + event.target.errorCode);
-			request.onsuccess = async (event) => {
-				this.db = event.target.result;
-				resolve({
-					db: this.db,
-					requestId,
-				});
-			};
-
-			// # upgrade
-			request.onupgradeneeded = async (event) => {
-				this.upgradeCallback(event);
-			};
-		});
+		let dbRequestResult = await this.dbConnection.OpenDb_();
+		return dbRequestResult;
 	}
 }
